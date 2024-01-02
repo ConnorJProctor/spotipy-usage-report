f26d3d9b7477028ba471a3faac62cf0ba639debe
Connor Proctor <connor.j.proctor@gmail.com>
2024-01-01 23:53:18 -0500

    Authentication configured and tested

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import constants
+import spotipy
+import requests
+from constants import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET
+from spotipy.oauth2 import SpotifyClientCredentials
+
+auth_manager = SpotifyClientCredentials(SPOTIPY_CLIENT_ID,SPOTIPY_CLIENT_SECRET)
+sp = spotipy.Spotify(auth_manager=auth_manager)
